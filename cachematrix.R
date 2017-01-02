bdc6e0c0b4517258f388a6a35cbb3914e12af458
akmomo <akram.almosleh@gmail.com>
2017-01-03 08:23:16 +1100

    added the solution for project assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# first function requied to create the list of fuctions 
+#
+MakeCacheMatrix<- function (x=numeric(),dimension)
+{ # the only command that will run when you call this function is the last - list()
+  # just to construct a list of switches of the function MakrCacheMatrix()
+    
+      
+        # sub-function to create the matrix in the global space across the board
+	  makematrix <- function(y=x, dim2=dimension)
+	  {
+	      X <<- matrix (y, nrow = dim2,ncol = dim2) 
+	      Xinv <<- NULL
+	  }
 
-## Write a short comment describing this function
+        
+	# sub-function to retrieve the matrix from the global variable
+          getmatrix <- function() {return(X)}
+			  
+	
+	# sub-function to set the inverse globally across the board
+	  setinv <- function(x) {Xinv <<- x}                                                               
 
-makeCacheMatrix <- function(x = matrix()) {
+	# sub-function to get the inverse from the global variable
+	  getinv <- function() {Xinv}
 
+	
+	# the only command that will run when you call Makecachematrix() is the next one
+	# just to construct a list the different switches of the function
+
+          list( makem = makematrix , getm =getmatrix , getinv =getinv , setinv =setinv)
+ }
+	
+#
+# second function required to retrieve the inverse from the global variable Xinv 
+# if Xinv is nulled due to change in the matrix
+#
+CacheSolve <- function(Z)
+{
+  #find if the inv is computed already
+   if (!is.null(Z$getinv()))
+      {
+              message("Retrieving cached data")
+	      return (Xinv)
+      }
+
+  #else solve for the inverse - use solve()!!   
+
+      message("Hang on... solving the matrix")
+      data1 <- Z$getm()	            
+      data1inv <- solve(data1)
+      Z$setinv(data1inv)
+      # return the newly computed inverse to the screen
+      data1inv
+						      	
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
